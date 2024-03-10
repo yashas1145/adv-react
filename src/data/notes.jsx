e477506be568dcabed3fbf50858600be906c2ace
Yashas BN <yashas1145@gmail.com>
2024-03-10 08:50:17 +0530

    Adding more dummy data

--- src/data/notes.jsx
+++ src/data/notes.jsx
@@ -22,6 +22,12 @@ const notes = [
       title: "Hardware vs. Software",
       content:
         "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+    },
+    {
+      key: 5,
+      title: "Clean code vs. Dummy code",
+      content:
+        "Clean code always is more sensible than horribly indented code."
     }
 ];
 
