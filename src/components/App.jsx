3c588752a02341704a5dbc87e3b77495d5c4b0ad
Yashas BN <yashas1145@gmail.com>
2024-03-18 11:37:04 +0530

    Minr updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../data/notes";
-import Login from "./Login";
-
-var loggedIn = true;
-var registered = false;
-
-function renderNote () {
-    return <div className="note-container">
-        {notes.map((note) => {
-            return <Note key={note.key} title={note.title} content={note.content}/>
-        })}
-    </div>
-}
-
-function renderForm() {
-    return <div>
-        <Login registered={registered}/>
-    </div>
-}
+import notesFromJson from "../data/notes";
 
 function App () {
+    const [notes, setNotes] = useState(notesFromJson);
+    const [note, setNote] = useState({"key": notes.length+1,"title": "", "content": ""});
+
+    function handleInputChange (event) {
+        const {name, value} = event.target;
+        setNote(prevNote => ({...prevNote, [name]: value}));
+        console.log(note);
+    }
+
+    function handleClick () {
+        setNotes(prevNotes => [...prevNotes, note]);
+        setNote({"key": notes.length+1, "title": "", "content": ""});
+    }
+
     return <div>
         <Header/>
-        {loggedIn ? renderNote() : renderForm()}
+        <div className="note-container">
+            {notes.map((note) => {
+                return <Note key={note.key} title={note.title} content={note.content}/>
+            })}
+            <div className="note">
+                <input onChange={handleInputChange} name="title" type="text" placeholder="Note title" value={note.title}/>
+                <input onChange={handleInputChange} name="content" type="text" placeholder="Note description" value={note.desc}/>
+                <button onClick={handleClick}>Add note</button>
+            </div>
+        </div>
         <Footer/>
     </div>
 }
